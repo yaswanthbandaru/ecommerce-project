233c48ff2a3900b0da583df099addff10910a33e
Yaswanth   Bandaru <yaswanth.bandaru@dhan.ai>
2024-09-26 01:13:10 +0530

    Added logger to the project

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
-const express = require('express')
 const post_graphile = require('./postgraphile')
-import { Timestamp } from "typeorm"
+const express = require('express')
+import { Request, Response, NextFunction } from "express"
 import { AppDataSource } from "./data-source"
+import { logger } from "./logger"
 
 const app = express()
 const port = 4000
 
 AppDataSource.initialize()
-    .then(async () => console.log("Connected to the database ..."))
-    .catch( error => console.log("Error while connecting to database:", error))
+    .then(async () => logger.info("Data Source has been initialized!"))
+    .catch( error => logger.error("Error while connecting to database:", error))
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    logger.info(`Incoming request: ${req.method} ${req.url}`),
+    res.on('finish', () => { logger.info(`Response status: ${res.statusCode}`) }),
+    next()
+})
 
 app.use(post_graphile)
 
 app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}/`)
+    logger.info(`Server is listening at http://localhost:${port}/`)
 })
